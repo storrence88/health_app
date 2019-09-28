c74a685dbde248ccf84a9e2c5e09b9f8f7895b04
Steven Torrence <storrence88@gmail.com>
2019-09-28 16:01:02 -0500

    Fix rubocop errors

--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :entry do
     user { nil }
-    date { "2019-09-28" }
-    weight { "9.99" }
+    date { '2019-09-28' }
+    weight { '9.99' }
   end
 end
