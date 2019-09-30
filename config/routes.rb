83f79cefc19f7be4d2ffa54e2d20b35625ca303f
Steven Torrence <storrence88@gmail.com>
2019-09-29 19:44:55 -0500

    Import bootstrap and create users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'users#index'
+  
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'signup' }
+  
+  authenticated :user do
+    resource :users
+    root 'users#index'
+  end
+
+  # root 'welcome#index'
 end
